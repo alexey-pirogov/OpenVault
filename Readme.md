4e02f9fa02cd0f769719ef9fda77d6c11a070301
alexey-pirogov <pirogov30002@gmail.com>
2024-06-24 02:51:28 +0300

    update: configure backend

--- Readme.md
+++ Readme.md
@@ -1,6 +1,39 @@
 # Open Vault
 
+A file hosting service that you can quickly deploy on your server.
+
+# Configuring
+
+Create `.env` file for web application:
+
+```
+SECRET_KEY=ergierufhw894fhw49fh8w49fw4fwfoiejf
+API_URL=127.0.0.1
+API_PORT=80
+POSTGRES_PORT=5432
+POSTGRES_HOST=db
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=postgres
+POSTGRES_DB=database
+
+```
+
+Create `.env.db` file for database:
+
+```
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=postgres
+POSTGRES_DB=database
+
+```
+
+## Quickstart
+
+Build & run project with docker-compose:
+
+```bash
+sudo docker-compose up -d --build
+```
+
 > [!NOTE]
-> Файлообменник, который можно быстро развернуть на своём сервере.
-
-
+> To access the documentation for the REST API, go to the application's URL `http://<url>/api/swagger`.
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,12 +1,23 @@
+import os
+from os import environ
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-aa5jz+uvibh$fua3bpxa@5x9i0#)ul6gbaxluixgh%cg3d95j4'
+SECRET_KEY = environ.get('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+HOST = environ.get('API_URL')
+
+PORT = environ.get('API_PORT')
+
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "db",
+    HOST,
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -15,7 +26,47 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
+    'drf_yasg',
+    'rest_framework.authtoken',
+    #'django_celery_beat',
 ]
+
+AUTH_USER_MODEL = 'api.Profile'
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    f'http://{HOST}:{PORT}',
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DATE_FORMAT': "%d.%m.%Y",
+    'TIME_FORMAT': "%H:%M",
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
+    ],
+}
+
+SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': False,
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -49,8 +100,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': environ.get('POSTGRES_DB'),
+        'USER': environ.get('POSTGRES_USER'),
+        'PASSWORD': environ.get('POSTGRES_PASSWORD'),
+        'HOST': environ.get('POSTGRES_HOST'),
+        'PORT': environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
@@ -69,14 +124,32 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = STATIC_DIR
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+
+# CELERY_BEAT_SCHEDULE = {
+#     'restore-user-tokens': {
+#         'task': 'api.tasks.restore_user_tokens_balance',
+#         'schedule': 60,  # задаем время в секундах
+#     },
+# }
